68cb2813acfa4e91e2ed107da2cf9a35de581998
Your Name <you@example.com>
2025-04-22 00:34:05 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// your code goes here
+$('.content').each( function(i){
+  
+    var bottom_of_object= $(this).offset().top + $(this).outerHeight();
+    var bottom_of_window = $(window).height();
+    
+    if( bottom_of_object > bottom_of_window){
+      $(this).addClass('hidden');
+    }
+});
+  
+  
+$(window).scroll( function(){
+    $('.hidden').each( function(i){
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+    
+        if( bottom_of_window > bottom_of_object ){
+            $(this).animate({'opacity':'1'},700);
+        }
+    });
+});
